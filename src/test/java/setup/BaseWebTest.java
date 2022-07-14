6def35b9f9b186125b8babcc72fc605b0be9f068
Gabriel Cespedes <gabrielcespedes@192.168.1.8>
2022-07-13 21:27:32 -0300

    Added headless support

--- src/test/java/setup/BaseWebTest.java
+++ src/test/java/setup/BaseWebTest.java
@@ -3,6 +3,7 @@ package setup;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -29,8 +30,12 @@ public class BaseWebTest {
     }
 
     public void startBrowser(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--window-size=1400,800");
         logInfo("Starting browser");
-        webDriver = new ChromeDriver();
+        webDriver = new ChromeDriver(chromeOptions);
     }
 
     public void closeBrowser(){
